4350d8768dda5409adba1eaf6fda9216efebc83c
ra388 <62772095+ra388@users.noreply.github.com>
2020-04-10 16:56:40 +0900

    Add files via upload

---
+++ hw/vowel.js
@@ -0,0 +1,8 @@
+let a="When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be.";
+
+
+for (let i = 0 ; i <a.length ; ++i){
+    if (a[i] == "a" || a[i] == "e" || a[i] =="i" || a[i] == "o" || a[i] =="u"){
+        console.log(i);
+    }
+}
